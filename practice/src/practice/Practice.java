82845a25d77fc4edd74e2eb4613e3209a8150902
Yahya-zakiri <152258827+Yahya-zakiri@users.noreply.github.com>
2025-10-22 16:28:22 +0430

    Add files via upload

---
+++ practice/src/practice/Practice.java
@@ -0,0 +1,12 @@
+package practice;
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        double raduis = 3;
+        double area = 3.14 * raduis *raduis;
+        
+        System.out.println("The area of this circle is: " + area);
+        }
+}
